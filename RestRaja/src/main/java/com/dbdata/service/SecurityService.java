8647ada32ebd44d015b5f68e133f4bdaa4987fee
biist <72079769+JanneOhtonen@users.noreply.github.com>
2022-11-29 14:18:12 +0200

    autowirea

--- RestRaja/src/main/java/com/dbdata/service/SecurityService.java
+++ RestRaja/src/main/java/com/dbdata/service/SecurityService.java
@@ -2,7 +2,6 @@ package com.dbdata.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
