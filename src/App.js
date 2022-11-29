28a5ee9b94fd3a4463948129e8bb14f82193a9dc
LeeviIlli <t1ille00@students.oamk.fi>
2022-11-29 12:49:47 +0200

    ...

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./App.css";
+import React from "react";
+import { ReactChild } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Settings from "./components/Settings";
+import Test from "./Chart/Test";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/Test" element={<Test />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
