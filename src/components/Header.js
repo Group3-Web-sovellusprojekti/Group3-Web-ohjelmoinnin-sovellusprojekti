bde34b97a0480034a9cad2dcded05f15db4f8837
LeeviIlli <t1ille00@students.oamk.fi>
2022-12-08 15:39:44 +0200

    init

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 export default function Header() {
   return (
     <div class="header">
       <h1 class="headername">Nettisivu :D</h1>
+      <h2></h2>
     </div>
   );
 }
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,13 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = app => {
-    app.use(
-        createProxyMiddleware('/src/Chart/annglobal.js', //uus proxy uudelle chartille eli muuten sama mutta vaihtaa oikian reitin filee ja laittaa sen tuohon module.exportin  sisää
-        {
-            target: 'http://localhost:8080',
-            changeOrigin: true
-        })
+module.exports = (app) => {
+  app.use(
+    createProxyMiddleware(
+      "/src/Chart/annglobal.js", //uus proxy uudelle chartille eli muuten sama mutta vaihtaa oikian reitin filee ja laittaa sen tuohon module.exportin  sisää
+      {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      }
     )
-}
+  );
+};
