d9fdd622edc026f5df0a7d78f479e9d9d364f99e
biist <72079769+JanneOhtonen@users.noreply.github.com>
2022-12-08 15:05:20 +0200

    cleaned useless imports

--- RestRaja/src/main/java/com/dbdata/service/SecurityService.java
+++ RestRaja/src/main/java/com/dbdata/service/SecurityService.java
@@ -1,8 +1,5 @@
 package com.dbdata.service;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
