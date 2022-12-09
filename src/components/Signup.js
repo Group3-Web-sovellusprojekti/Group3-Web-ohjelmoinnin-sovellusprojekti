c93155b62f4e870ba12a0ecf792a34f9a7c6aa1f
LeeviIlli <t1ille00@students.oamk.fi>
2022-12-09 14:48:20 +0200

    init

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,18 +1,22 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const navigate = useNavigate();
+
   const handleSignupSubmit = async (event) => {
     event.preventDefault();
-    console.log(event.target.username.value);
-    console.log(event.target.password.value);
+    console.log(event.target.uname.value);
+    console.log(event.target.pw.value);
 
     try {
       const result = await axios.post("http://localhost:8080/register", {
-        username: event.target.username.value,
-        password: event.target.password.value,
+        username: event.target.uname.value,
+        password: event.target.pw.value,
       });
       console.log(result);
+      navigate("/login", { replace: true });
     } catch (error) {
       console.error(error);
     }
@@ -27,11 +31,11 @@ export default function SignUp() {
           <form onSubmit={handleSignupSubmit}>
             <div>
               username <br />
-              <input type="text" name="username" />
+              <input type="text" name="uname" />
             </div>
             <div>
               Password <br />
-              <input type="text" name="password" />
+              <input type="text" name="pw" />
             </div>
 
             <button type="submit" class="btn-1">
